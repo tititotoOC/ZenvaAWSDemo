df0fcad62d21b471a9ecae461af3cbeb4e00c28b
Thibault Bezpalko <thibault_bezpalko@hotmail.com>
2019-06-23 16:16:23 +0200

    small change

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.set('view engine', 'hjs')
 
 
 app.listen(5000)
-console.log('Server running on http://localhost:5000 - HELLO WORLD!')
+console.log('Server running on http://localhost:5000')
 
 console.log(process.env.DB_HOST)
